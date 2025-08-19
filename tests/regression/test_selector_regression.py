cd8884f581489e134ec6edb87d4e0a058bccf4b0
[dyad] <simon.skaldervikeninvest@gmail.com>
2025-08-20 00:27:39 +0200

    [dyad] Refactoring the entire repository file structure - wrote 18 file(s), renamed 17 file(s), deleted 4 file(s) + extra files edited outside of Dyad

---
+++ tests/regression/test_selector_regression.py
@@ -0,0 +1,6 @@
+import pytest
+
+@pytest.mark.regression
+def test_selector_regression_stub():
+    # TODO: Implement selector regression tests against a golden set of URLs
+    pytest.skip("Selector regression test not implemented.")
---
+++ tests/unit/test_db_manager.py
@@ -0,0 +1,6 @@
+import pytest
+
+@pytest.mark.unit
+def test_db_manager_stub():
+    # TODO: Implement database manager tests
+    pass
